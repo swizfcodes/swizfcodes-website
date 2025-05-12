7630823e2efb225dfb4d3b5d829edb8796da8876
swizfcodes <elishagodwin897@gmail.com>
2025-05-12 17:06:43 +0100

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -84,4 +84,32 @@
 
   startShuffle();
 
+  //toogle icon navbar
+
+  //scroll sections
+  let sections = document.querySelectorAll('section');
+  let navLinks = document.querySelectorAll('my-portfolio-header nav a');
+
+  window.onscroll = () => {
+    sections.forEach(sec => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 100;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+
+      if(top >= offset && top < offset + height){
+        // active navbar links
+        navLinks.forEach(links => {
+          links.classList.remove('active');
+          document.querySelector('my-portfolio-header nav a[href*=' + id+']').classList.add('active')
+        })
+      }
+    });
+
+    // sticky header
+    let header = document.querySelector('my-portfolio-header')
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+  }
+
   
